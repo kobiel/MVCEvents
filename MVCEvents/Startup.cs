32432ab474e6e79f06d096d53806080fe6ec1d65
Kobi <kobieliasi@gmail.com>
2015-07-29 16:13:45 +0300

    Init

---
+++ MVCEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCEvents.Startup))]
+namespace MVCEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
