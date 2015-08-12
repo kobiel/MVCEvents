0c720be9a79e3c0813a262c46a893e07cdc539a8
ahuvab <ahuvabloy@gmail.com>
2015-08-12 19:44:23 +0300

    add models

---
+++ MVCEvents/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace MVCEvents.Models
+{
+    public class Event
+    {
+        public String IdEvent { get; set; }
+        public String Type { get; set; }
+        public DateTime Date { get; set; }
+        public List<Guest> GuestsList { get; set; }
+    }
+}
