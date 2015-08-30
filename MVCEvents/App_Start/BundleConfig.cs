52c847ec79f849077f0eb21f59a0d9a31f3be2a9
ahuvab <ahuvabloy@gmail.com>
2015-08-30 17:36:17 +0300

    some changes

--- MVCEvents/App_Start/BundleConfig.cs
+++ MVCEvents/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MVCEvents
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/new.css",
                       "~/Content/site.css"));
         }
     }
